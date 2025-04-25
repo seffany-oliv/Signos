f420edd17bc21801cbdafb9ae55f25dd9cf7fc66
seffany-oliv <steffany.oliv1@gmail.com>
2025-04-25 12:17:16 -0300

    commit inicial steffany

---
+++ Signo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Signo {
+
+    public static void main(String[] args) {
+        
+        //Declaração de variáveis
+        int mes, num;
+
+        //Criação instância do objeto de entrada
+        Scanner entrada = new Scanner(System.in);
+
+        //Apresentação
+        System.out.println("\n\t\t\t--Signos--\n");
+
+        //Entrada
+        System.out.println("Imforme o mês");
+        mes = entrada.nextInt();
+        System.out.println("Imforme o dia");
+        num = entrada.nextInt();
+
+        //Processamento
+
+
+        //Saída
+
+    }
+}
